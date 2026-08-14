ac231522920cd3ce3cb8cdfc6601d29625c94bbc
Rahul Raushan <rahulraushan137138@gmail.com>
2026-08-15 00:02:44 +0530

    Time: 684 ms (9.73%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
-select firstName,lastName,city,state from Person p Left join  Address a on
-p.personId= a.personId ;
+/* Write your PL/SQL query statement below */
+select firstName , lastName,city ,state from Person p , Address a where 
+p.personId= a.personId(+);
