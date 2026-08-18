6cd5f57e8b5c3fcc6f7bcfd56a3cb685a9c61c8e
Rahul Raushan <rahulraushan137138@gmail.com>
2026-08-19 00:05:29 +0530

    Time: 494 ms (71.93%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,4 +1,13 @@
 /* Write your PL/SQL query statement below */
+
+
+/* 
+
 select player_id, to_char(first_login , 'YYYY-MM-DD') as first_login from(
     select player_id, event_date as first_login , dense_rank() over (partition by player_id order by event_date asc) as dr from Activity 
 ) where dr=1;
+
+*/
+
+ select player_id ,to_char( min(event_date),'yyyy-mm-dd') as first_login from Activity group by player_id;
+ 
