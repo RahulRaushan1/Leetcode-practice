f9a2e64925dac41e4c708d58a1ca24ea4b125e12
Rahul Raushan <rahulraushan137138@gmail.com>
2026-08-21 23:56:30 +0530

    Time: 714 ms (12.23%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,13 @@
 /* Write your PL/SQL query statement below */
 
 
-select customer_number from(
+ /*
+ select customer_number from(
     select customer_number, count(*) as total_orders, dense_rank() over (order by count(*) desc) as rk from orders group by customer_number
 ) where rk =1;
+
+*/
+
+select customer_number from (
+    select customer_number, count(*) as customer_count from Orders group by customer_number order by customer_count desc
+) where rownum=1;
