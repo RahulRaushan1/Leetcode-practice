5d37c99e7069a5731b85744058a459ef8c0ec70c
Rahul Raushan <rahulraushan137138@gmail.com>
2026-09-03 23:15:52 +0530

    Time: 445 ms (24.59%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,6 +1,2 @@
-# Write your MySQL query statement below
-SELECT *
-FROM Cinema
-WHERE MOD(id, 2) = 1
-  AND description <> 'boring'
-ORDER BY rating DESC;
+/* Write your PL/SQL query statement below */
+select * from Cinema where mod(id,2)=1 and description != 'boring' order by rating desc;
